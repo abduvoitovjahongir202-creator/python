06878ae134eba33e41c2cfd3ed3e2b6ed93dc74d
Jaxongir <abduvoitovjahongir202@gmail.com>
2026-04-22 18:29:03 +0500

    uy ishi

---
+++ 3-2uyishi.py
@@ -0,0 +1,41 @@
+import os
+os.system("cls")
+
+# 1-misol
+# n = int(input("n ni kirit: "))
+# yigindi = 0
+# for i in range(1, n + 1):
+#     print(i, end=" ")
+#     yigindi += i
+# print(f"\nYig'indi: {yigindi}")
+# print("MANA SHUNAQA BO'LARKANMISH")
+
+# 2-misol
+# n = int(input("n ni kiritsang ham boladi lekin: "))
+# for i in range(n if n % 2 == 0 else n-1, 1, -2):
+#     print(i, end=" ")
+    
+# 3-misol
+# son = input("Sonni kiritvor: ")
+# for raqam in son:
+#     print(raqam, end=" ")
+
+# 4-msiol
+# n = int(input("n sonini kiritsang boldi: "))
+# for i in range(n if n % 2 == 0 else n-1, 1, -2):
+#     print(i, end=" ")
+    
+# 5-misol
+# for num in range(10, 100):
+#     is_tub = True
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             is_tub = False
+#             break
+#     if is_tub:
+#         print(num, end=" ") 
+
+# 6-misol
+# son = input("Sonni kiritib qoyarsan: ")
+# for raqam in son[::-1]:
+#     print(raqam, end=" ")
